49a68223c03ff28207b2b6cb7aaa72029349d418
matia-lee <matialee9@gmail.com>
2024-03-22 00:12:17 -0400

    release year

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 from parsing_scripts.dates_from_diary import grab_dates_from_diaries
+from parsing_scripts.diary_movie_release_year import grab_diary_movie_release_year
 from parsing_scripts.diary_movie_genres import grab_diary_movie_genres
 from parsing_scripts.diary_movie_directors import grab_diary_movie_director
 from parsing_scripts.diary_movie_cast import grab_diary_movie_cast
@@ -8,7 +9,7 @@ from parsing_scripts.diary_movie_rating import grab_diary_movie_rating
 # from parsing_scripts.diary_movie_liked import update_liked_movies_in_diary_df
 
 if 'diary_df' not in st.session_state:
-    st.session_state.diary_df = pd.DataFrame(columns=['title', 'watched_date', 'genres', 'director', 'cast', 'rating', 'liked'])
+    st.session_state.diary_df = pd.DataFrame(columns=['title', 'watched_date', 'release_year', 'genres', 'director', 'cast', 'rating', 'liked'])
 
 def fetch_and_display_films(username):
     if username and username != st.session_state.get('last_username', ''):
@@ -17,10 +18,11 @@ def fetch_and_display_films(username):
         st.session_state.diary_df = pd.DataFrame(columns=['title', 'watched_date', 'genres'])
 
         st.session_state.diary_df = grab_dates_from_diaries(username, st.session_state.diary_df)
-        st.session_state.diary_df = grab_diary_movie_genres(st.session_state.diary_df)
-        st.session_state.diary_df = grab_diary_movie_director(st.session_state.diary_df)
-        st.session_state.diary_df = grab_diary_movie_cast(st.session_state.diary_df)
-        st.session_state.diary_df = grab_diary_movie_rating(username, st.session_state.diary_df)
+        st.session_state.diary_df = grab_diary_movie_release_year(username, st.session_state.diary_df)
+        # st.session_state.diary_df = grab_diary_movie_genres(st.session_state.diary_df)
+        # st.session_state.diary_df = grab_diary_movie_director(st.session_state.diary_df)
+        # st.session_state.diary_df = grab_diary_movie_cast(st.session_state.diary_df)
+        # st.session_state.diary_df = grab_diary_movie_rating(username, st.session_state.diary_df)
         # st.session_state.diary_df = update_liked_movies_in_diary_df(username, st.session_state.diary_df)
         
         if not st.session_state.diary_df.empty:
